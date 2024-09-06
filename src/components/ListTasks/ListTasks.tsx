e14d59503eb9ea6bb84878b821d8593bcdadd5b4
Afonso Monteiro <affonsoh.monteiro@gmail.com>
2024-09-05 21:44:03 -0300

    criado taskList

--- src/components/ListTasks/ListTasks.tsx
+++ src/components/ListTasks/ListTasks.tsx
@@ -1,20 +1,28 @@
+import { Task } from '../Task/Task'
 import styles from './ListTasks.module.css'
 
 
 export function ListTasks() {
   return (
     <section className={styles.tasks}>
-      <header>
+      <header className={styles.header}>
         <div>
           <p>Tarefas criadas</p>
           <span>5</span>
         </div>
 
         <div>
-          <p>Concluídas</p>
+          <p className={styles.textPurple}>Concluídas</p>
           <span>2 de 5</span>
         </div>
       </header>
+      
+      <div className={styles.list}>
+        <Task />
+        <Task />
+        <Task />
+      </div>
+      
     </section>
   )
 }
---
+++ src/components/Task/Task.tsx
@@ -0,0 +1,18 @@
+import { TbTrash } from 'react-icons/tb'
+import styles from './Task.module.css'
+
+export function Task() {
+  return (
+    <div className={styles.task}>
+      <button className={styles.checkContainer}>
+        <div className={styles.check} />
+      </button>
+
+      <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+      
+      <button className={styles.deleteButton}>
+        <TbTrash size={20} />
+      </button>
+    </div>
+  )
+}
